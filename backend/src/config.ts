f191639614242f5e3336d2b72880bd4fb430bb4d
Mosacd <levanmosiashvili5@gmail.com>
2025-05-30 10:05:29 +0400

    initial types and config added

---
+++ backend/src/config.ts
@@ -0,0 +1,12 @@
+import { GameConfig } from './types';
+
+export const GAME_CONFIG: GameConfig = {
+  FIELD_WIDTH: 800,
+  FIELD_HEIGHT: 400,
+  PADDLE_HEIGHT: 80,
+  PADDLE_WIDTH: 10,
+  BALL_SIZE: 10,
+  BALL_INITIAL_SPEED: 5,
+  PADDLE_SPEED: 8,
+  WINNING_SCORE: 5
+};
